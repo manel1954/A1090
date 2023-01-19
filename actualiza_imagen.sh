1409e9b013b0062477120d0631d5dd0f9f3c6525
manel1954 <55663831+manel1954@users.noreply.github.com>
2023-01-19 01:43:44 +0100

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
+                        #Actualiza  macbook
+                        #cd /home/pi
+                        #sudo rm -R B108
+                        #git clone http://github.com/ea3eiz/D108
+                        #sleep 1
+                        #mv D108 B108
+                        #sleep 1
+                        #sudo chmod 777 -R B108
+                        #cd /home/pi/B108
+                        #./qt_imagen_actualizada
+                        #sudo reboot
+
+                        cd /home/pi/.local
+                        sudo rm -R A1090
+                        git clone http://github.com/manel1954/A1090
+                        sleep 2
+                        if [ -f /home/pi/.local/A1090/aviso ]
+                        then
+                        sudo rm -R /home/pi/A108
+                        cp -R /home/pi/.local/A1090 /home/pi
                         cd /home/pi
-                        sudo rm -R A108
-                        git clone http://github.com/ea3eiz/A108NEW
-                        sleep 1
-                        mv A108NEW A108
-                        sleep 1
+                        mv A1090 A108
                         sudo chmod 777 -R A108
+                        cd /home/pi/.local
+                        sudo rm -R A1090
+
                         cd /home/pi/A108
                         ./qt_imagen_actualizada
-                        
-
-                        #cd /home/pi/.local
-                        #sudo rm -R A108NEW
-                        #git clone http://github.com/ea3eiz/A108NEW
-                        #sleep 2
-                        #if [ -f /home/pi/.local/A108NEW/aviso ]
-                        #then
-                        #sudo rm -R /home/pi/A108
-                        #cp -R /home/pi/.local/A108NEW /home/pi
-                        #cd /home/pi
-                        #mv A108NEW A108
-                        #sudo chmod 777 -R A108
-                        #cd /home/pi/.local
-                        #sudo rm -R A108NEW
-                        #else
-                        #clear
-                        #echo "${VERDE}"
-                        #echo "***********************************"
-                        #echo -n "${ROJO}"
-                        #echo "           ERROR DE RED            "
-                        #echo -n "${VERDE}"
-                        #echo "***********************************"
-                        #sudo rm -R /home/pi/.local/A108MEW
-                        #echo "${GRIS}"
-                        #sleep 5
-                        #exit
-                        #fi
-
-
-
-
+                        else
+                        clear
+                        echo "${VERDE}"
+                        echo "***********************************"
+                        echo -n "${ROJO}"
+                        echo "           ERROR DE RED            "
+                        echo -n "${VERDE}"
+                        echo "***********************************"
+                        sudo rm -R /home/pi/.local/A1090
+                        echo "${GRIS}"
+                        sleep 5
+                        exi
