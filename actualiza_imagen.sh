a0ed852f38784c0da764cdbda9ea04a049afa6f8
manel1954 <55663831+manel1954@users.noreply.github.com>
2023-01-19 17:51:05 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
                         cd /home/pi/.local
-                        git clone http://github.com/manel1954/A1090
+                        git clone http://github.com/manel1954/D1090
                         sleep 2
-                        if [ -f /home/pi/.local/A1090/aviso ]
+                        if [ -f /home/pi/.local/D1090/aviso ]
                         then
                         sudo rm -R /home/pi/A108
-                        cp -R /home/pi/.local/A1090 /home/pi
+                        cp -R /home/pi/.local/D1090 /home/pi
                         cd /home/pi
-                        mv A1090 A108
+                        mv D1090 A108
                         sudo chmod 777 -R A108
                         cd /home/pi/.local
-                        sudo rm -R A1090
+                        sudo rm -R D1090
 
                         cd /home/pi/A108
                         ./qt_imagen_actualizada
@@ -22,7 +22,7 @@
                         echo "           ERROR DE RED            "
                         echo -n "${VERDE}"
                         echo "***********************************"
-                        sudo rm -R /home/pi/.local/A1090
+                        sudo rm -R /home/pi/.local/D1090
                         echo "${GRIS}"
                         sleep 5
                         exit
