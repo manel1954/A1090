3c30a6dfd970a4e5877b6febcea73a0649253176
manel1954 <55663831+manel1954@users.noreply.github.com>
2023-01-19 02:14:21 +0100

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-                        #Actualiza  macbook
-                        #cd /home/pi
-                        #sudo rm -R B108
-                        #git clone http://github.com/ea3eiz/D108
-                        #sleep 1
-                        #mv D108 B108
-                        #sleep 1
-                        #sudo chmod 777 -R B108
-                        #cd /home/pi/B108
-                        #./qt_imagen_actualizada
-                        #sudo reboot 
-
                         cd /home/pi/.local
                         sudo rm -R A1090
                         git clone http://github.com/manel1954/A1090
