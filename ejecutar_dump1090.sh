c2a21ed6af0db58fc3bae0ddc7c50f4480956035
manel1954 <55663831+manel1954@users.noreply.github.com>
2023-01-26 00:10:51 +0100

    Update ejecutar_dump1090.sh

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -16,9 +16,14 @@ stick=$(awk "NR==40" /home/pi/status.ini)
 puerto=$(awk "NR==42" /home/pi/status.ini)
 ppm=$(awk "NR==44" /home/pi/status.ini)
 http=$(awk "NR==46" /home/pi/status.ini)
+gain=$(awk "NR==48" /home/pi/status.ini)
 
 if [ "$stick" = 'RSP1' ];then
 xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --dev-sdrplay --net-ro-port $puerto --ppm $ppm --net-http-port $http
+else
+
+elif [ "$gain" = '-10' ];then
+xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --net-ro-port $puerto --gain $gain --ppm $ppm --net-http-port $http
 else
 xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --net-ro-port $puerto --ppm $ppm --net-http-port $http
 fi
