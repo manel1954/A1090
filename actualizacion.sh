f7fe68f8439e8488adc811a59c52d8091754211c
Manel <ea3eiz@ea3eiz.com>
2023-02-03 22:48:15 +0100

    Update actualizacion.sh

--- actualizacion.sh
+++ actualizacion.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
                         
                         actualizacion=$(awk "NR==1" /home/pi/.local/actualizacion.txt)
-
                         if [ $actualizacion = "1.1.0" ];then
                         echo "no hace nada"
                         else
                         echo "Hay una actualización"
-                        read a
-                        
+                        cd /home/pi/A108
+                        ./qt_editor_bm                        
                         fi
