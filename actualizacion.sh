44734de94f3a67ae1946b9b317937cb6ba3dda7c
Manel <ea3eiz@ea3eiz.com>
2023-02-03 22:19:06 +0100

    Update actualizacion.sh

--- actualizacion.sh
+++ actualizacion.sh
@@ -2,7 +2,7 @@
                         
                         actualizacion=$(awk "NR==1" /home/pi/.local/actualizacion.txt)
 
-                        if [ $actualizacion = "1.0.0" ];then
+                        if [ $actualizacion = "1.1.0" ];then
                         echo "no hace nada"
                         else
                         echo "Hay una actualización"
