a312a54cb4ddd0bb3171cef5b7c6ea0181c40bbd
Manel <ea3eiz@ea3eiz.com>
2023-02-17 18:38:33 +0100

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-                        #Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-                        
-                        
+#!/bin/bash                       
                         cd /home/pi/.local
                         git clone http://github.com/manel1954/D1090
                         if [ -f /home/pi/.local/D1090/aviso ]
