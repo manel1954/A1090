e8b44df56cf21a31371dc100ef82e9d64c1b0a16
Manel <ea3eiz@ea3eiz.com>
2023-02-17 16:36:12 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -2,7 +2,6 @@
                         cd /home/pi/.local
                         git clone http://github.com/manel1954/D1090
                         sleep 2
-                        clear
                         if [ -f /home/pi/.local/D1090/aviso ]
                         then
                         sudo rm -R /home/pi/A108
@@ -12,7 +11,6 @@
                         sudo chmod 777 -R A108
                         cd /home/pi/.local
                         sudo rm -R D1090
-                        clear
                         cd /home/pi/A108
                         ./qt_imagen_actualizada
                         else
