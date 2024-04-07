187c7a4af7081cb5e7e8f36c73abd4c0547ef004
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-04-07 17:00:53 +0200

    Update instalar_radiosonde.sh

--- instalar_radiosonde.sh
+++ instalar_radiosonde.sh
@@ -3,9 +3,7 @@ sudo apt-get install python3 python3-venv sox git build-essential libtool cmake 
 sudo apt-get install libusb-1.0-0-dev rng-tools libsamplerate-dev libatlas3-base libgfortran5 libopenblas-dev -y
 
 cd ~
-
-rm -R ~/radiosonde_auto_rx/auto_rx
-
+rm -R ~/radiosonde_auto_rx/
 git clone https://github.com/projecthorus/radiosonde_auto_rx.git
 cd radiosonde_auto_rx/auto_rx
 ./build.sh
