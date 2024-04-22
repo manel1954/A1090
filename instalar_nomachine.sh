5248549683aec20133420f12e408e06f11b49074
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-04-22 19:55:57 +0200

    Update instalar_nomachine.sh

--- instalar_nomachine.sh
+++ instalar_nomachine.sh
@@ -10,7 +10,7 @@ GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
                         # Comprueba si NOMACHINE está instalado
-                        estado_nomachine=$(awk "NR==31" /home/pi/status.ini)
+                        estado_nomachine=$(awk "NR==66" /home/pi/status.ini)
                         if [ "$estado_nomachine" = 'NOMACHINE=ON' ];then
                         echo "\v\v\v\v\v\v"
                         echo "${ROJO}"
@@ -41,7 +41,7 @@ MARRON="\33[38;5;138m"
                         echo "                  SE HA INSTALADO NOMACHINE CON EXITO                    "
                         echo "***********************************************************************" 
                         echo "***********************************************************************"                      
-                        sed -i "31c NOMACHINE=ON" /home/pi/status.ini                        
+                        sed -i "66c NOMACHINE=ON" /home/pi/status.ini                        
                         sleep 3
                         fi                       
                        
